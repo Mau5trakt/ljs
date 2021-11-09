3d7b3776230531b1969d11544706205ebcc6628d
Mau5trakt <kubzalemany@hotmail.com>
2021-11-09 16:22:35 -0600

    Type of operations in JavaScript

---
+++ bloque1/operaciones.js
@@ -0,0 +1,32 @@
+//asignación
+var a = 1;
+var b = 2; 
+
+//suma 
+var suma = a+b; 
+
+//resta 
+var resta = a-b;
+
+//multiplicacion
+var mult = a*b;
+
+//division
+var div = a/b;
+
+//modulo 
+var mod = a%b;
+
+//exponenciacion 
+
+var exp = a**b;
+
+console.log("A:",a, "incrementación:", a += 1);
+
+//Texto 
+
+nombre = "Enrique";
+apellido = "Alemany";
+
+console.log(nombre+" "+apellido);
+
