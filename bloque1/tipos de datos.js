14b77ee80c5be5745f55923012ea342ae00ad428
Mau5trakt <kubzalemany@hotmail.com>
2021-11-09 19:30:28 -0600

    knowing about the diferent data type in JS

---
+++ bloque1/tipos de datos.js
@@ -0,0 +1,15 @@
+var lista = ["aa","bb", 3,4,5, [1, "a"]]
+
+console.log(lista[1]);
+
+var nombreFuncion = function(){
+var a = 2;
+var b = 4; 
+
+console.log(a+b);
+}
+
+
+nombreFuncion()
+nombreFuncion()
+nombreFuncion()
