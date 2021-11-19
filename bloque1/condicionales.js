fcfee1278e73a77bba16da9152c39d51adae343f
Mau5trakt <kubzalemany@hotmail.com>
2021-11-18 19:51:32 -0600

    learning vectores and conditionals

---
+++ bloque1/condicionales.js
@@ -0,0 +1,8 @@
+var edad  =  15;
+
+if (edad > 15){
+  console.log("Ta chiquito")
+}  else {
+  console.log("no ta chiquito")
+}
+
---
+++ bloque1/vectores.js
@@ -0,0 +1,12 @@
+var  array  = ["aa", "bb" , [1,2]];
+var objeto ={
+  edad: 6
+}
+
+var item0 = array[0]
+
+console.log(item0)
+
+var  longitud = array.length
+
+console.log(longitud)
