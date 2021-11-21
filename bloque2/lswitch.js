bce6a8906b5dc59b0e04f47304d3fff9225b17dd
Mau5trakt <kubzalemany@hotmail.com>
2021-11-21 17:59:54 -0600

    learning about switch in js

---
+++ bloque2/lswitch.js
@@ -0,0 +1,16 @@
+// El switch viene siendo un else/if pero más óptimo 
+var status = 2;
+switch(status){
+
+  case  0:
+    console.log("vale  0")
+    break;
+
+  case 1:
+    console.log("vale  1")
+    break;
+
+  case 2:
+    console.log("vale  2")
+    break;
+}
