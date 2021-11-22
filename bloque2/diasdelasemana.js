ffb4b2300601f78e24b04dbbb6bd246adb2959ea
Mau5trakt <kubzalemany@hotmail.com>
2021-11-21 18:00:20 -0600

    excercise about switch using in js

---
+++ bloque2/diasdelasemana.js
@@ -0,0 +1,51 @@
+var mes  =  "Febrero"
+switch(mes){//Meses con 31 días 
+  case "Enero":
+    console.log("El mes de ", mes ,"tiene 31 días ")
+    break;
+
+  case "Marzo":
+    console.log("El mes de ", mes ,"tiene 31 días ")
+    break;
+
+  case "Mayo":
+    console.log("El mes de ", mes ,"tiene 31 días ")
+    break;
+
+  case "Julio":
+    console.log("El mes de ", mes ,"tiene 31 días ")
+    break;
+
+  case "Agosto":
+    console.log("El mes de ", mes ,"tiene 31 días ")
+    break;
+
+  case "Octubre":
+    console.log("El mes de ", mes ,"tiene 31 días ")
+    break;
+
+  case "Diciembre":
+    console.log("El mes de ", mes ,"tiene 31 días ")
+    break;
+    //Meses con 30 dias
+
+  case "Abril":
+    console.log("El mes de ", mes ,"tiene  30  días")
+    break;
+
+  case "Junio":
+    console.log("El mes de ", mes ,"tiene  30  días")
+    break;
+
+  case "Septiembre":
+    console.log("El mes de ", mes ,"tiene  30  días")
+    break;
+
+  case "Noviembre":
+    console.log("El mes de ", mes ,"tiene  30  días")
+    break;
+    //Febrero
+  case  "Febrero":
+    console.log("El mes de ", mes ,"tiene 28 días")
+    break;
+}
